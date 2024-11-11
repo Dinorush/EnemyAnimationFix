a5b30ef050ab027a3bc6f331f0044f0100966a2f
Dinorush <onparnorthstar@gmail.com>
2024-11-10 23:00:01 -0500

    Fix wave enemies AFKing

---
+++ EnemyAnimationFix/Patches/EnemyTargetPatches.cs
@@ -0,0 +1,25 @@
+﻿using Enemies;
+using HarmonyLib;
+using static Enemies.EnemyUpdateManager;
+
+namespace EnemyAnimationFix.Patches
+{
+    [HarmonyPatch]
+    internal static class EnemyTargetPatches
+    {
+        [HarmonyPatch(typeof(EnemyUpdateManager), nameof(EnemyUpdateManager.MasterMindUpdateFixed))]
+        [HarmonyPostfix]
+        private static void PostManagerUpdate(EnemyUpdateManager __instance)
+        {
+            ForceIntervalUpdate(__instance.m_detectionUpdatesClose);
+            ForceIntervalUpdate(__instance.m_detectionUpdatesNear);
+            ForceIntervalUpdate(__instance.m_detectionUpdatesFar);
+        }
+
+        private static void ForceIntervalUpdate(UpdateDetectionGroup group)
+        {
+            foreach (var behaviour in group.m_members)
+                behaviour.m_ai.m_enemyAgent.m_validTargetInterval = 0;
+        }
+    }
+}
