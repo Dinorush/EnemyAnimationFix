5f8e657afba473f636d06ae0d81734ad655c3847
Dinorush <onparnorthstar@gmail.com>
2024-12-26 18:42:16 -0500

    Potential glue fix

---
+++ EnemyAnimationFix/Patches/EnemyGluePatches.cs
@@ -0,0 +1,25 @@
+﻿using Enemies;
+using HarmonyLib;
+using SNetwork;
+
+namespace EnemyAnimationFix.Patches
+{
+    [HarmonyPatch]
+    internal static class EnemyGluePatches
+    {
+        [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.ReceiveGlueDamage))]
+        [HarmonyPostfix]
+        private static void SyncGlueData(Dam_EnemyDamageBase __instance, pMiniDamageData data)
+        {
+            if (SNet.IsMaster)
+                __instance.m_glueDamagePacket.Send(data, SNet_ChannelType.GameNonCritical);
+        }
+
+        [HarmonyPatch(typeof(ES_StuckInGlue), nameof(ES_StuckInGlue.ActivateState))]
+        [HarmonyPrefix]
+        private static bool StopClientStateSwitch()
+        {
+            return SNet.IsMaster;
+        }
+    }
+}
