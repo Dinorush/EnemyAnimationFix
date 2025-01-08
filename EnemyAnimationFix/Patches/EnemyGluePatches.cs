5547cd629ae2e44335c5e56122aec0195ca8caec
Dinorush <onparnorthstar@gmail.com>
2025-01-07 21:30:01 -0500

    Fix glue for clients if host has it

--- EnemyAnimationFix/Patches/EnemyGluePatches.cs
+++ EnemyAnimationFix/Patches/EnemyGluePatches.cs
@@ -7,12 +7,16 @@ namespace EnemyAnimationFix.Patches
     [HarmonyPatch]
     internal static class EnemyGluePatches
     {
-        [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.ReceiveGlueDamage))]
+        [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.AddToTotalGlueVolume))]
         [HarmonyPostfix]
-        private static void SyncGlueData(Dam_EnemyDamageBase __instance, pMiniDamageData data)
+        private static void SyncGlueData(Dam_EnemyDamageBase __instance, GlueGunProjectile? proj, GlueVolumeDesc volume)
         {
-            if (SNet.IsMaster)
-                __instance.m_glueDamagePacket.Send(data, SNet_ChannelType.GameNonCritical);
+            if (!SNet.IsMaster) return;
+
+            pMiniDamageData data = default;
+            float mod = proj != null ? proj.EffectMultiplier : 1f;
+            data.damage.Set((volume.volume + volume.expandVolume) * mod, 100f);
+            __instance.m_glueDamagePacket.Send(data, SNet_ChannelType.GameNonCritical);
         }
 
         [HarmonyPatch(typeof(ES_StuckInGlue), nameof(ES_StuckInGlue.ActivateState))]
