8e12286379bdd7715aa435dc5f64c49f86f6b525
Dinorush <onparnorthstar@gmail.com>
2025-01-16 17:24:10 -0500

    Version bump

--- EnemyAnimationFix/EntryPoint.cs
+++ EnemyAnimationFix/EntryPoint.cs
@@ -7,7 +7,7 @@ using HarmonyLib;
 
 namespace EnemyAnimationFix
 {
-    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.2.0")]
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.2.1")]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
     internal sealed class EntryPoint : BasePlugin
     {
