65b3fca6ae555f54b9e1f0188930bd5dc7406e88
Dinorush <onparnorthstar@gmail.com>
2025-08-05 20:38:50 -0400

    Prevent animation override if host does not have fix

--- EnemyAnimationFix/Patches/EnemyAnimPatches.cs
+++ EnemyAnimationFix/Patches/EnemyAnimPatches.cs
@@ -1,4 +1,5 @@
 ﻿using Enemies;
+using EnemyAnimationFix.Networking.Notify;
 using HarmonyLib;
 
 namespace EnemyAnimationFix.Patches
@@ -10,7 +11,7 @@ namespace EnemyAnimationFix.Patches
         [HarmonyPrefix]
         private static void FixGiantScream(ES_Scream __instance, ref pES_EnemyScreamData data)
         {
-            if (__instance.m_locomotion.AnimHandleName != EnemyLocomotion.AnimatorControllerHandleName.EnemyGiant) return;
+            if (!NotifyManager.MasterHasFix || __instance.m_locomotion.AnimHandleName != EnemyLocomotion.AnimatorControllerHandleName.EnemyGiant) return;
 
             data.AnimIndex = 0;
         }
@@ -20,7 +21,7 @@ namespace EnemyAnimationFix.Patches
         [HarmonyPrefix]
         private static void FixFiddlerAttack(ES_EnemyAttackBase __instance, ref int attackIndex)
         {
-            if (__instance.m_locomotion.AnimHandleName != EnemyLocomotion.AnimatorControllerHandleName.EnemyFiddler) return;
+            if (!NotifyManager.MasterHasFix || __instance.m_locomotion.AnimHandleName != EnemyLocomotion.AnimatorControllerHandleName.EnemyFiddler) return;
 
             attackIndex %= 2;
         }
@@ -29,7 +30,7 @@ namespace EnemyAnimationFix.Patches
         [HarmonyPrefix]
         private static void FixLowStagger(ES_Hitreact __instance, ref int index, ES_HitreactType hitreactType, ImpactDirection impactDirection)
         {
-            if (__instance.m_locomotion.AnimHandleName != EnemyLocomotion.AnimatorControllerHandleName.EnemyLow || hitreactType != ES_HitreactType.Light || impactDirection != ImpactDirection.Back) return;
+            if (!NotifyManager.MasterHasFix || __instance.m_locomotion.AnimHandleName != EnemyLocomotion.AnimatorControllerHandleName.EnemyLow || hitreactType != ES_HitreactType.Light || impactDirection != ImpactDirection.Back) return;
 
             index %= 3;
         }
