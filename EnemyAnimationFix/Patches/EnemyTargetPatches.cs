051486e4552038435975381259694b89ac07a863
Dinorush <onparnorthstar@gmail.com>
2025-11-02 13:24:46 -0500

    Fix remote door break

--- EnemyAnimationFix/Patches/EnemyTargetPatches.cs
+++ EnemyAnimationFix/Patches/EnemyTargetPatches.cs
@@ -1,5 +1,6 @@
 ﻿using Enemies;
 using HarmonyLib;
+using System;
 namespace EnemyAnimationFix.Patches
 {
     [HarmonyPatch]
@@ -19,6 +20,25 @@ namespace EnemyAnimationFix.Patches
         {
             if (__instance.m_machine.CurrentState.ENUM_ID != (byte)EB_States.InCombat && __instance.m_ai.IsTargetValid && __instance.m_ai.Target.m_nodeDistance <= 1)
                 __instance.TryUpdateNavigation(EnemyCourseNavigationMode.MoveToNextNode);
+        }
+
+        [HarmonyPatch(typeof(EB_InCombat_MoveToNextNode_DestroyDoor), nameof(EB_InCombat_MoveToNextNode_DestroyDoor.UpdateBehaviour))]
+        [HarmonyPrefix]
+        private static void RemoteDoorFix(EB_InCombat_MoveToNextNode_DestroyDoor __instance, ref IntPtr __state)
+        {
+            var navigation = __instance.m_ai.m_courseNavigation;
+            __state = navigation.IsPathBlocked ? navigation.m_navPortal.Pointer : IntPtr.Zero;
+        }
+
+        [HarmonyPatch(typeof(EB_InCombat_MoveToNextNode_DestroyDoor), nameof(EB_InCombat_MoveToNextNode_DestroyDoor.UpdateBehaviour))]
+        [HarmonyPostfix]
+        private static void RemoteDoorFixPost(EB_InCombat_MoveToNextNode_DestroyDoor __instance, IntPtr __state)
+        {
+            if (__state == IntPtr.Zero) return;
+
+            var navigation = __instance.m_ai.m_courseNavigation;
+            if (navigation.IsPathBlocked && navigation.m_navPortal.Pointer != __state)
+                __instance.m_machine.ChangeState((byte)EB_States.InCombat_MoveToNextNode_PathBlocked);
         }
     }
 }
